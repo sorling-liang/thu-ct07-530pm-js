d04010aa7032574c1edb32a6049f556ddec59577
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-16 10:49:56 +0000

    Jul 16, 2026, 10:49 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -29,7 +29,7 @@ function setup() {
   // --- Exercise: Age category classification ---
   // write your codes here
   let myAge = 11;
-  // junior
+  // junior 
 
   // --- Exercise: Display odd numbers backward using while loop ---
   // write your codes here

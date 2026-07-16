1ce3fdc7435f2e2fbccd0b8510589362819fef75
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-16 10:28:05 +0000

    Jul 16, 2026, 10:28 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -14,7 +14,7 @@ function setup() {
   text("the area of a triangle is "+ areaTriangle, 50, 100);
 
   for (let i=3; i <=46; i+=3) {
-    text(i, 50, )
+    text(i, 50, 120+(i*10));
   }
   // --- Exercise: Sum of first 10 even numbers ---
   // write your codes here

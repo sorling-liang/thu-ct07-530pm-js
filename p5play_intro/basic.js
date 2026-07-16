79942e7c18381539c76a0e08424596db51c70f51
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-16 10:45:01 +0000

    Jul 16, 2026, 10:45 AM

--- p5play_intro/basic.js
+++ p5play_intro/basic.js
@@ -13,7 +13,7 @@ function setup() {
   textSize(16);
   text("the area of a triangle is "+ areaTriangle, 50, 100);
 
-  for (let i=3; i <=46 ; i+=2) {
+  for (let i=2; i <=20 ; i+=2) {
     //   sometext, x,  y
     //text(i,      50, 120+(i*10));
     console.log(i);

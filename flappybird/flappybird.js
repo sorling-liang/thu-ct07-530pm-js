faa9117994cbaf50b7d269b296035c51598d7c8f
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-23 10:42:35 +0000

    Jul 23, 2026, 10:42 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -4,7 +4,7 @@ let flapMidImg, bg, base;
 
 function preload() {
     // bird image, background, and the floor
-    flapMidImg = loadImage('assets/');
+    flapMidImg = loadImage('assets/yellow');
 }
 
 

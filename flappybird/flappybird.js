d186dbc53dd4a909efa37143d9411bfe14ab32a3
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-30 09:47:23 +0000

    Jul 30, 2026, 9:47 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -22,7 +22,7 @@ function setup() {
 
     bird.collider = "dynamic";
     bird.mass = 2;
-    bird.drag = 
+    bird.drag = 0.02;
     bird.bounciness = 0.5; // stop here slide 19 lesson 4
 
     let floor = new Sprite();

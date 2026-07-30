e365ef617d05a4ec3fcdeaac538ec93fc70ca73c
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-30 10:25:12 +0000

    Jul 30, 2026, 10:25 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -65,7 +65,7 @@ function draw() {
         bird.img = flapUpImg;
         bird.rotation = -30;
     }
-    else if (bird.vel.y < -1) {
+    else if (bird.vel.y > 1) {
         bird.img = flapDownImg;
         bird.rotation = 30;
     }

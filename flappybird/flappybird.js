9f0adaf750a0bb6d8b7c5a949e2d249681afa4dd
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-30 09:35:28 +0000

    Jul 30, 2026, 9:35 AM

---
+++ flappybird/flappybird.js
@@ -0,0 +1,37 @@
+// the final game
+let bird, floor;
+let flapMidImg, bg, base;
+
+function preload() {
+    // bird image, background, and the floor
+    flapMidImg = loadImage('assets/yellowbird-midflap.png');
+    bg = loadImage('assets/background-day.png');
+    base = loadImage('assets/base.png');
+}
+
+function setup() {
+    new Canvas(400, 600);
+    background("white");
+
+    bird = new Sprite();
+    bird.x = width /2;
+    bird.y = 200;
+    bird.width = 30;
+    bird.height = 30;
+    bird.img = flapMidImg;
+
+    bird.collider = "dynamic";
+    bird.bounciness = 0.5; // stop here slide 19 lesson 4
+
+    let floor = new Sprite();
+    floor.width = 400;
+    floor.y = 500;
+    floor.collider = "static";
+
+    world.gravity.y = 10; // gravity pulls the sprite
+}
+
+function draw() {
+    // test if bg is okay
+    image(bg, 0, 0, width, height); // draw the background
+}

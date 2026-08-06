552afc73a699b1d73f2d851f8cfedfd6ad0a6e88
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-06 10:10:26 +0000

    Aug 6, 2026, 10:10 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -117,7 +117,7 @@ function spawnPipePair() {
 
     someY = midY - gap/2 - 200;
 
-    topPipe = new Sprite(400, someY, 52, 320, 'static');
+    topPipe = new Sprite(bird.x + 400, someY, 52, 320, 'static');
     topPipe.img = pipe;
     topPipe.rotation = 180; // flipped over
 

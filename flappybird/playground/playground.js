3bfa5ce8af95c3a65cc2ea0137aa6114bf44a459
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-13 10:16:02 +0000

    Aug 13, 2026, 10:16 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -8,7 +8,7 @@ let flapMidImg, base;
 
 // task 1: write 3 functions needed for p5play
 function preload() {
-    f
+    flapMidImg = loadImage("assets/yellowbird-")
 }
 
 // task 2: create a canvas

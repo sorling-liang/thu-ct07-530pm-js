9d5783c1b779ad1c55fedf78db1ee6419056ffb5
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-13 09:51:40 +0000

    Aug 13, 2026, 9:51 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -38,7 +38,7 @@ function setup() {
     bird.height = 25;
     bird.img = flapMidImg;
 
-    bird.collider = "dynamic";
+    bird.collider = "static"; // "dynamic";
     bird.mass = 2; // weight
     bird.drag = 0.02; // air resistance
     bird.bounciness = 0.5; // does it bounce or not?

caff7e6bca1ccfe873ab73fdffde42a96fd3f84c
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-13 09:56:03 +0000

    Aug 13, 2026, 9:56 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -38,8 +38,8 @@ function setup() {
     bird.height = 25;
     bird.img = flapMidImg;
 
-    bird.collider = "static"; // "dynamic";
-    bird.visible = false; // hide the bird
+    // bird.collider = "static"; // "dynamic";
+    // bird.visible = false; // hide the bird
 
     bird.mass = 2; // weight
     bird.drag = 0.02; // air resistance
@@ -75,13 +75,13 @@ function draw() {
     text("pipeGroup.length = " + pipeGroup.length, 20, 100);
 
     // wait for the player to start the game by interaction
-    if ( kb.presses("space") || mouse.presses() ) {
-        startGame = true;
-        startScreenLabel.visible = false;
-        // revert the bird
-        bird.visible = true;
-        bird.collider = "dynamic"; // the bird can start falling
-    }
+    // if ( kb.presses("space") || mouse.presses() ) {
+    //     startGame = true;
+    //     startScreenLabel.visible = false;
+    //     // revert the bird
+    //     bird.visible = true;
+    //     bird.collider = "dynamic"; // the bird can start falling
+    // }
 
     if (!startGame) {
         return; // statement will not continue the next line

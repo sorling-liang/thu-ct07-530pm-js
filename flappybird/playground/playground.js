4f7870d5f2b55bc72394df935fd877df5b358fc5
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-13 10:46:35 +0000

    Aug 13, 2026, 10:46 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -50,7 +50,7 @@ function draw() {
     text("frameCount= " + frameCount, 50, 50);
     text("canvas width= " + width,    50, 70);
     text("canvas height= " + height,  50, 90);
-    text("frameCount= " + frameCount, 50, 110);
+    text("bird.sleeping= " + bird.slee, 50, 110);
     
     if (kb.presses("space")) {
         bird.sleeping = false;

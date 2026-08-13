aa72288ba747aad783e6e6b1535947c8eea7fcb6
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-13 09:39:48 +0000

    Aug 13, 2026, 9:39 AM

--- flappybird/flappybird.js
+++ flappybird/flappybird.js
@@ -67,7 +67,7 @@ function draw() {
     text("pipeGroup.length = " + pipeGroup.length, 20, 100);
 
 
-    startScreenLabel = new Sprite(x, y, w, h);
+    startScreenLabel = new Sprite(width/2, height/2, w, h);
 
 
 

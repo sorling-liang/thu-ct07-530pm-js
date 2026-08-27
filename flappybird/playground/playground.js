310195262333a95f64d537ccd4a75791774eb373
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-27 09:53:57 +0000

    Aug 27, 2026, 9:53 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -19,14 +19,7 @@ function preload() {
 // called once
 function setup() {
     new Canvas(400, 600);
-
-    // digitLabel = new Sprite();
-    // digitLabel.y = 50;
-    // digitLabel.width = 25; // sprite.w
-    // digitLabel.height = 36; // sprite.h
-    // digitLabel.collider = "none";
-    //digitLabel.img = digitImgs[score]; // load the correct image using the array
-
+    
     scoreGroup = new Group();
     scoreGroup.collider = "none";
 }

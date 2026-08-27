215c5521aacd5fb6c0ff4259cf6fe5baf24e7cc0
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-27 10:48:00 +0000

    Aug 27, 2026, 10:48 AM

--- flappybird/playground/playground.js
+++ flappybird/playground/playground.js
@@ -2,7 +2,7 @@
 function setup() {
     new Canvas(600,400);
     background("lightgray");
-    
+
     world.gravity.y = 10;
 }
 
@@ -10,6 +10,6 @@ function draw() {
     background("lightgray");
 
     if (mouse.presses()) {
-        new Sprite();
+        let s = new Sprite();
     }
 }

f0e846ba0e0a5059ea31b023050379e701bc43b1
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-09-24 10:03:51 +0000

    Sep 24, 2026, 10:03 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -63,6 +63,7 @@ function sliceFruit() {
         // dist(): calculate distance
         let distance = dist(mouse.x, mouse.y, fruit.x, fruit.y); // is this fruit near the mouse pointer?
         let hitRadius = fruit.diameter/2 + 5;
+        
         if (distance < hitRadius) {
             fruit.sliced = true; // i am slicing this one
 

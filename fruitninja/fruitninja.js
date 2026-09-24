a35bf126e92727f836a43c210b5d66f7fdfd4167
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-09-24 10:44:21 +0000

    Sep 24, 2026, 10:44 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -67,7 +67,14 @@ function splitFruit(xpos, ypos, fruitType) {
     fruitHalves.add(left); // add to group
 
     // you do spawn right half
-    let right = new Sprite(xpos-10, ypos, 35);
+    let right = new Sprite(xpos+10, ypos, 35);
+    left.img = fruitType.half1;
+    left.vel.x = -3; // veer left
+    left.vel.y = random(-5, -2);
+    left.rotationSpeed = -5;
+    left.life = 30; // 30 frames so half a second
+
+    fruitHalves.add(left); // add to group
 }
 // cut the fruit using the mouse pressed (or dragged across the canvas)
 function sliceFruit() {

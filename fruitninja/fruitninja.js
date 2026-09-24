c96dbe44bc0054fd575eda76af52abe512d86fe1
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-09-24 09:46:38 +0000

    Sep 24, 2026, 9:46 AM

--- fruitninja/fruitninja.js
+++ fruitninja/fruitninja.js
@@ -43,8 +43,8 @@ function draw() {
 
     if (mouse.pressing()) {
         // left mouse button dragged
-        let tail = new Sprite(mouseX, mouseY, 7);
-        //tail.stroke = "red";
+        let tail = new Sprite(mouseX, mouseY, 77);
+        tail.stroke = "red";
         tail.color = "red";
         tail.collider = "none";
         tail.life = 25; // lifetime measured in frameCount for that dot
